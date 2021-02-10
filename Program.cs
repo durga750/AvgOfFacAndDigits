325328ecc726d6ded9e7c100f399a4d484368831
durga750 <78526756+durga750@users.noreply.github.com>
2021-02-10 15:25:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 12;
+            int sum = 0;
+            int count = 0;
+            int avg = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i );
+                    count++;
+                    sum = sum + i;
+                    avg = sum / count;
+                  
+                }
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(count);
+            Console.WriteLine(avg);
+        }
+    }
+}
